cc0152b33e80d34c0890247e7bb854f8c98dec1b
yudihartadi <50376616+yudihartadi@users.noreply.github.com>
2019-07-16 14:12:01 +0700

    Yudi.js
    Kerad

--- autoclick.js
+++ autoclick.js
@@ -1,10 +1,10 @@
 let isRecaptchaFrame = () => {
-  return /https:\/\/www.google.com\/recaptcha\/api2\/anchor/.test(window.location.href);
+  return /https:Btcspinner.io./Home\/\/www.google.com\/recaptcha\/api2\/anchor/.test(window.location.href);
 };
 
 let captchaInterval = setInterval(() => {
   if (isRecaptchaFrame()) {
     clearInterval(captchaInterval);
-    document.getElementsByClassName('recaptcha-checkbox-checkmark')[0].click();
+    document.getElementsByClassName('recaptcha-checkbox-checkmark')[1].click();
   }  
 }, 500);
